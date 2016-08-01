bf205e53d46a8622811d1be9e92dbbaacb84628d
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2016-08-01 18:56:19 +0200

    inital commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("nav").find("a").click(function(e) {
+      e.preventDefault();
+      var section = $(this).attr("href");
+      $("html, body").animate({
+          scrollTop: $(section).offset().top
+      });
+  });
+});
