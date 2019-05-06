44bd59af7f25592cb8368409ec926db6a61dd5a3
Simon Hintersonnleitner <simon@hintersonnleitner.me>
2019-05-06 22:59:50 +0200

    Fix: update website with gulp output

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $("nav").find("a").click(function(e) {
-      e.preventDefault();
-      var section = $(this).attr("href");
-      $("html, body").animate({
-          scrollTop: $(section).offset().top
-      });
-  });
-});
